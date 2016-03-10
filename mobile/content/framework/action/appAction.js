29bd816b444b26b89855a9989003714cd25a5a06
lianjie chen <lchen@amarsoft.com>
2016-03-10 10:34:58 +0800

    add upload module using react native add  host file to avoid git confilt at fetch file refactor the upload handle optmize code format for action file

--- mobile/content/framework/action/appAction.js
+++ mobile/content/framework/action/appAction.js
@@ -1,4 +1,4 @@
-var {XFetch,PFetch,upload,host,token} = require('../network/fetch');
+var {BFetch,PFetch,UFetch,host,token} = require('../network/fetch');
 var async = require('async')
 var Push = require('../network/push');
 var AppDispatcher = require('../dispatcher/appDispatcher');
@@ -14,55 +14,44 @@ var Actions = {
     registerAPNS: ()=>Push.init(),
     sendSMSCodeToNewMobile: (p, c, f)=>PFetch(pub + "/sendSMSCodeToNewMobile", p, c, f),
     sendSMSCodeToOldMobile: (p, c, f)=>PFetch(pub + "/sendSMSCodeToOldMobile", p, c, f),
-    sendSMSCodeToNewMobileApi: (p, c, f)=>XFetch(api + "/User/sendSMSCodeToNewMobile", p, c, f),
+    sendSMSCodeToNewMobileApi: (p, c, f)=>BFetch(api + "/User/sendSMSCodeToNewMobile", p, c, f),
     validateSMSCode: (p, c, f)=>PFetch(pub + "/validateSMSCode", p, c, f),
     register: (p, c, f)=>_register(pub + "/register", p, c, f),
     appInit: ()=>_appInit(),
     login: (p, c, f)=>_login(pub + "/login", p, c, f),
     logOut: ()=>_logout(api + "/User/logout"),
-    validateMobileForReg: (p, c, f)=>XFetch(pub + "/validateMobileForReg", p, c, f),
-    validateMobileForForgetPwd: (p, c, f)=>XFetch(pub + "/validateMobileForForgetPwd", p, c, f),
-    resetPasswordForForgetPwd: (p, c, f)=>XFetch(pub + "/resetPasswordForForgetPwd", p, c, f),
+    validateMobileForReg: (p, c, f)=>BFetch(pub + "/validateMobileForReg", p, c, f),
+    validateMobileForForgetPwd: (p, c, f)=>BFetch(pub + "/validateMobileForForgetPwd", p, c, f),
+    resetPasswordForForgetPwd: (p, c, f)=>BFetch(pub + "/resetPasswordForForgetPwd", p, c, f),
     updateUser: (p, c, f)=>_updateUser(api + "/User/updateUser", p, c, f),
-    validatePassword: (p, c, f)=>XFetch(api + "/User/validatePassword", p, c, f),
-    validateMobileForResetMobile: (p, c, f)=>XFetch(api + "/User/validateMobileForResetMobile", p, c, f),
+    validatePassword: (p, c, f)=>BFetch(api + "/User/validatePassword", p, c, f),
+    validateMobileForResetMobile: (p, c, f)=>BFetch(api + "/User/validateMobileForResetMobile", p, c, f),
     resetMobileNo: (p, c, f)=>_resetMobileNo(api + "/User/resetMobileNo", p, c, f),
-    resetPasswordForChangePwd: (p, c, f)=>XFetch(api + "/User/resetPasswordForChangePwd", p, c, f),
+    resetPasswordForChangePwd: (p, c, f)=>BFetch(api + "/User/resetPasswordForChangePwd", p, c, f),
     updateUserHead: (p, c, f)=> _updateUserHead(p, c, f),
     updateCompBaseInfo: (p, c, f)=> _updateCompBaseInfo(p, c, f),
     submitOrg: (p, c, f)=> _submitOrg(p, c, f),
-    clear: ()=> {
-        AppDispatcher.dispatch({
-            type: ActionTypes.LOGOUT,
-        });
-    },
-    forceLogOut: ()=> {
-        AppDispatcher.dispatch({
-            type: ActionTypes.FORCE_LOGOUT
-        });
-    },
     cancleBillDiscount: (p, c)=>_cancleBillDiscount(api + "/BillDiscountAdmin/cancleBillDiscount", p, c),
     createBillDiscount: (p, c)=>_createBillDiscount(api + "/BillDiscountAdmin/createBillDiscount", p, c),
     giveUpBillDiscount: (p, c)=>_giveUpBillDiscount(api + "/BillDiscountAdmin/giveUpBillDiscount", p, c),
     getStdOrg: (p, c, f)=>PFetch(api + '/Organization/getStdOrg', p, c, f, {custLoading: true}),
-    sendSMSCodeForDiscount: (p, c, f)=>XFetch(api + "/User/sendSMSCodeToOldMobile", p, c, f),
+    sendSMSCodeForDiscount: (p, c, f)=>BFetch(api + "/User/sendSMSCodeToOldMobile", p, c, f),
     validateMobileForDiscount: (p, c, f)=>PFetch(api + "/User/validateSMSCodeAfterLogin", p, c, f),
     clearMessageDetail: (p)=>_clearMessageDetail(p),
     setOtherMsgRead: (p, c, f) => PFetch(api + "/MessageSearch/setOtherMsgRead", p, c, f),
     setBillRevRead: (p, c, f) => PFetch(api + "/MessageSearch/setBillRevRead", p, c, f),
-    feedbackOpinion: (p, c, f)=>XFetch(api + "/User/feedbackOpinion", p, c, f),
+    feedbackOpinion: (p, c, f)=>BFetch(api + "/User/feedbackOpinion", p, c, f),
     downloadCertificate: ()=>_downloadCertificate(),
     getFile: (fid)=>host + api + '/File/downLoad/' + fid + '?token=' + token(),
     getProtocol: ()=>host + '/protocol.html',
     getRegion: (f, e)=>_getRegion(f, e),
     startRPC: (option)=>_startRPC(option),
     endRPC: (option, handle)=>_endRPC(option, handle),
-    setDemoFlag: ()=> {
-        AppDispatcher.dispatch({
-            type: ActionTypes.DEMO_FLAG
-        });
-    }
+    setDemoFlag: ()=> {AppDispatcher.dispatch({type: ActionTypes.DEMO_FLAG});},
+    clear: ()=> {AppDispatcher.dispatch({type: ActionTypes.LOGOUT,});},
+    forceLogOut: ()=> {AppDispatcher.dispatch({type: ActionTypes.FORCE_LOGOUT});}
 }
+
 var _clearMessageDetail = function (p) {
     AppDispatcher.dispatch({
         type: ActionTypes.CLEAR_MESSAGEDETAIL,
@@ -77,36 +66,39 @@ var _updateCompBaseInfo = function (p, c, f) {
         successHandle: c
     });
 }
-var _updateUserHead = function (p, c, f) {
-    var uploadURL = api + '/File/uploadFile'
-    var uploadFileHandle = function (name) {
-        return function (callback) {
-            if (p[name].length == 24) {
+
+var uploadFileHandle = function (params, fileFieldName) {
+    return function (callback) {
+        UFetch(api + '/File/uploadFile',
+            {
+                uri: params[name],
+                type: 'image/jpeg',
+                name: name,
+            },
+            function (data) {
                 callback(null, name);
-            } else {
-                upload(uploadURL, p[name],
-                    function (data) {
-                        PFetch(api + '/User/updateUserPhoto', {photoStoreId: data.fileId},
-                            function () {
-                                AppDispatcher.dispatch({
-                                    type: ActionTypes.UPDATE_USERINFO,
-                                    data: p,
-                                    successHandle: c
-                                });
-                            });
-                    },
-                    function (err) {
-                        callback(err, name);
-                    });
-            }
-        }
+            },
+            function (err) {
+                callback(err, name);
+            });
     }
-    async.series([uploadFileHandle('photoStoreId')],
+}
+
+var _updateUserHead = function (p, c, f) {
+    async.series([uploadFileHandle(p,'photoStoreId')],
         function (err, res) {
             if (err) {
                 f();
             } else {
-                c();
+                Actions.updateUser(
+                    {photoStoreId: data.fileId},
+                    function(data){
+                        
+                    },
+                    function(err){
+                        
+                    }
+                )
             }
         }
     );
@@ -114,7 +106,7 @@ var _updateUserHead = function (p, c, f) {
 var _updateUser = function (u, p, c, f) {
     var key = _.keys(p)[0];
     var value = p[key]
-    XFetch(u, {column: key, value: value},
+    BFetch(u, {column: key, value: value},
         function (msg) {
             AppDispatcher.dispatch({
                 type: ActionTypes.UPDATE_USERINFO,
@@ -127,7 +119,7 @@ var _updateUser = function (u, p, c, f) {
 var _resetMobileNo = function (u, p, c, f) {
     var key = _.keys(p)[0];
     var value = p[key]
-    XFetch(u, p,
+    BFetch(u, p,
         function (msg) {
             AppDispatcher.dispatch({
                 type: ActionTypes.UPDATE_USERINFO,
@@ -138,7 +130,7 @@ var _resetMobileNo = function (u, p, c, f) {
     )
 }
 var _logout = function (url, c) {
-    XFetch(url, {},
+    BFetch(url, {},
         function () {
             AppDispatcher.dispatch({
                 type: ActionTypes.LOGOUT,
@@ -149,7 +141,7 @@ var _logout = function (url, c) {
 
 var _login = function (url, p, c, f) {
 
-    XFetch(url, p,
+    BFetch(url, p,
         function (msg) {
             AppDispatcher.dispatch({
                 type: ActionTypes.LOGIN,
@@ -163,7 +155,7 @@ var _login = function (url, p, c, f) {
 
 var _register = function (url, p, c, f) {
 
-    XFetch(url, p,
+    BFetch(url, p,
         function (msg) {
             AppDispatcher.dispatch({
                 type: ActionTypes.LOGIN,
@@ -189,7 +181,7 @@ var _cancleBillDiscount = function (url, p, c) {
 }
 
 var _createBillDiscount = function (url, p, c) {
-    XFetch(url, p,
+    BFetch(url, p,
         function (msg) {
             AppDispatcher.dispatch({
                 type: ActionTypes.CREATE_BILL_DISCOUNT,
@@ -212,37 +204,19 @@ var _giveUpBillDiscount = function (url, p, c) {
     );
 }
 var _submitOrg = function (p, c, f) {
-
-    var uploadURL = api + '/File/uploadFile'
-    var uploadFileHandle = function (name) {
-        return function (callback) {
-            if (p[name].length == 24) {
-                callback(null, name);
-            } else {
-                upload(uploadURL, p[name],
-                    function (data) {
-                        _updateCompBaseInfo({[name]: data.fileId})
-                        callback(null, name);
-                    },
-                    function (err) {
-                        callback(err, name);
-                    })
-            }
-        }
-    }
     async.series([
-            uploadFileHandle('licenseCopyFileId'),
-            uploadFileHandle('orgCodeCopyFileId'),
-            uploadFileHandle('taxFileId'),
-            uploadFileHandle('corpIdentityFileId'),
-            uploadFileHandle('authFileId'),
-            uploadFileHandle('authIdentityFileId'),
+            uploadFileHandle(p,'licenseCopyFileId'),
+            uploadFileHandle(p,'orgCodeCopyFileId'),
+            uploadFileHandle(p,'taxFileId'),
+            uploadFileHandle(p,'corpIdentityFileId'),
+            uploadFileHandle(p,'authFileId'),
+            uploadFileHandle(p,'authIdentityFileId'),
         ],
         function (err, res) {
             if (err) {
                 f();
             } else {
-                XFetch(api + "/Organization/updateOrg", p,
+                BFetch(api + "/Organization/updateOrg", p,
                     function (data) {
                         _updateCompBaseInfo({
                             biStatus: 'CERTIFIED',
@@ -257,12 +231,6 @@ var _submitOrg = function (p, c, f) {
             }
 
         })
-
-}
-
-var _downloadCertificate = function (p) {
-
-
 }
 
 var _notificationRegister = function (token) {
@@ -274,13 +242,8 @@ var _notificationRegister = function (token) {
 }
 
 var _onNotification = function (notification) {
-
-    //if(notification==undefined){
-    //    console.log("app state is change");
-    //    return;
-    //}
-    //  var msg = JSON.parse(notification.getMessage());
-    XFetch(api + "/MessageSearch/getPushMsg", {}, function (data) {
+    
+    BFetch(api + "/MessageSearch/getPushMsg", {}, function (data) {
         AppDispatcher.dispatch({
             type: ActionTypes.PUSH_NOTIFICATION,
             data: data
