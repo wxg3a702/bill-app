92f8ded2ab175a5094151a1f23d670dd836c18d4
liushengwei <swliu@amarsoft.com>
2016-03-11 09:55:39 +0800

    解决了一个报错问题

--- mobile/content/comp/utils/rightTopButton.js
+++ mobile/content/comp/utils/rightTopButton.js
@@ -12,16 +12,14 @@ var RightTopButton = React.createClass({
     getDefaultProps() {
         return {
             content: '',
-            color:'#44bcbc'
+            color: '#44bcbc'
         }
     },
     render(){
         return (
             <View>
-                <TouchableOpacity onPress={this.logout} style={{padding:10}} activeOpacity={0.5}
-                                  style={{flexDirection:'row',alignItems:'center',height:20}}>
-                    <Image resizeMode="stretch" style={{width:20,height:20}}
-                           source={this.props.source}/>
+                <TouchableOpacity onPress={this.logout} activeOpacity={0.5} style={styles.layout}>
+                    <Image resizeMode="stretch" style={{width:20,height:20}} source={this.props.source}/>
                     <Text style={[{color:this.props.color},styles.font]}>{this.props.content}</Text>
                 </TouchableOpacity>
             </View>
@@ -33,6 +31,13 @@ var styles = StyleSheet.create({
         textAlign: 'right',
         marginRight: 5,
         fontSize: 15
+    },
+    layout: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: 20,
+        padding: 10
     }
+
 })
 module.exports = RightTopButton;
