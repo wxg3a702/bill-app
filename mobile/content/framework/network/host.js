f4d4d8c8628ec62e0e8cf38d43976b5764b0cc76
xgwen <xgwen@amarsoft.com>
2016-03-21 10:34:13 +0800

    代码合并

--- mobile/content/framework/network/host.js
+++ mobile/content/framework/network/host.js
@@ -2,6 +2,7 @@
 //var host = "http://192.168.64.205:9101";
 //var host="http://192.168.64.252:9081";
 
-var host = 'http://192.168.64.205:8585/zxbilldev';
+//var host = 'http://192.168.64.205:8585/zxbilldev';
+var host = 'http://192.168.64.205:8484/zxbilldev';
 
 module.exports = host
