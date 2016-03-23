e9e92e7c96221a2bc207e74f7d6246d5936ae7d5
alcat2008 <ge_yangyang@126.com>
2016-03-23 22:01:32 +0800

    Recreate persister's schema.

--- mobile/content/framework/persister/persisterFacade.js
+++ mobile/content/framework/persister/persisterFacade.js
@@ -1,95 +1,190 @@
-var React = require('react-native');
-var {
-    AsyncStorage,
-    } = React;
+const _ = require('lodash');
+const Realm = require('realm');
+const SCHEMA_DEVICE = 'device';
+const SCHEMA_TOKEN = 'token';
+const SCHEMA_VALUE = 'value';
+const SCHEMA_FLAG = 'flag';
 
-var Actions = {
-    getAppData: (cb)=> {
-        _getAppData(cb)
-    },
-    saveAppData: (data)=> {
-        _saveAppData(data)
-    },
-    clearToken: ()=>_clearToken(),
-    setItem: (k, v, c)=>_setItem(k, v, c),
-    saveUser: (p, c)=>_saveUser(p, c),
-    saveOrg: (p, c)=>_saveOrg(p, c),
-    saveNewOrg: (p, c)=>_saveNewOrg(p, c),
-    saveAPNSToken: (p)=>_saveAPNSToken(p),
-    saveMsgDetail: (p)=>_saveMsgDetail(p),
-    saveDemoFlag: (p)=>_saveDemoFlag(p),
-    saveMainMsgBean: (p)=>_saveMainMsgBean(p)
-    //getUnReadnum:
-}
-//?
+const DEVICE_ID = '-device-id-';
+let DeviceSchema = {
+    name: SCHEMA_DEVICE,
+    primaryKey: 'device',
+    properties: {
+        device: {type: 'string'},
+        APNSToken: {type: 'string', optional: true}
+    }
+};
+
+let ValueSchema = {
+    name: SCHEMA_VALUE,
+    primaryKey: 'key',
+    properties: {
+        key: {type: 'string'},
+        value: {type: 'string'}
+    }
+};
+
+let FlagSchema = {
+    name: SCHEMA_FLAG,
+    primaryKey: 'key',
+    properties: {
+        key: {type: 'string'},
+        flag: {type: 'bool'}
+    }
+};
+
+let TokenSchema = {
+    name: SCHEMA_TOKEN,
+    primaryKey: 'token',
+    properties: {
+        token: {type: 'string'},
+        APNSToken: {type: 'string', optional: true},
+        values: {type: 'list', objectType: SCHEMA_VALUE},
+        flags: {type: 'list',  objectType: SCHEMA_FLAG}
+    }
+};
 
 
-var _saveUser = function (user, cb) {
-    _setItem('userInfoBean', user, cb);
-}
+// Get the default Realm with support for our objects
+//let _realm = new Realm({schema: [DeviceSchema, ValueSchema, FlagSchema, TokenSchema]});
+let _realm = new Realm({schema: [DeviceSchema, ValueSchema, FlagSchema, TokenSchema]});
+let _token = '';
 
-var _saveNewOrg = function (user, cb) {
-    _setItem('newOrg', user, cb);
-}
+let _clearToken = function () {
+    _realm.write(() => {
+        // Delete multiple books by passing in a `Results`, `List`,
+        // or JavaScript `Array`
+        let _persister = _realm.objects(SCHEMA_TOKEN);
+        _realm.delete(_persister); // Deletes all books
+    });
+};
+_clearToken();
 
-var _saveOrg = function (user, cb) {
-    _setItem('certifiedOrgBean', user, cb);
-}
-
-var _saveAPNSToken = function (data, cb) {
-    _setItem('APNSToken', data, cb);
-}
-var _saveMsgDetail = function (data, cb) {
-    _setItem('mainMsgBean', data, cb);
-}
-var _saveMainMsgBean = function (data, cb) {
-    _setItem('mainMsgBean', data, cb);
-}
-var _clearToken = function () {
-    AsyncStorage.removeItem("token", function (err) {
-    })
-}
-
-var _saveDemoFlag = function (flag, cb) {
-    _setItem('demoFlag', flag, cb);
-}
-
-var _setItem = function (key, value, cb) {
-    AsyncStorage.setItem(key, JSON.stringify(value), function (err) {
+let _setValue = function (key, value, cb) {
+    _realm.write(() => {
+        _realm.create(TokenSchema, {
+            token: _token,
+            values: [{
+                key: key,
+                value: JSON.stringify(value)
+            }]
+        }, true);
         if (cb)cb();
+    });
+};
 
-    })
-}
+let _setFlag = function (key, flag, cb) {
+    _realm.write(() => {
+        _realm.create(TokenSchema, {
+            token: _token,
+            flags: [{
+                key: key,
+                flag: Boolean(flag)
+            }]
+        }, true);
+        if (cb)cb();
+    });
+};
 
 
-var _getAppData = function (cb) {
-    AsyncStorage.multiGet(['token', 'APNSToken', 'revBillBean', 'sentBillBean', 'filterBeans', 'userInfoBean', 'certifiedOrgBean'
-        , 'mainMsgBean', 'marketMsgBeans', 'systemMsgBeans', 'sentBillMsgBeans', 'demoFlag', 'newOrg']).then(
-        (data) => {
-            var dataJson = {};
-            data.map((item, index)=> {
-                dataJson[item[0]] = JSON.parse(item[1])
-            })
-            if (cb)cb(dataJson);
+let _getAppData = function (cb) {
+    let tokenData = _realm.objects(SCHEMA_TOKEN);
+    let deviceData = _realm.objects(SCHEMA_DEVICE);
+    let _appObject = {};
+
+    if (deviceData.length > 0) {
+        let _persisterDevice = deviceData[0];
+        _.assign(_appObject, {
+            APNSToken: _persisterDevice.APNSToken,
         });
-}
+    }
 
-var _saveAppData = function (data) {
-    AsyncStorage.multiSet([
-        ["revBillBean", JSON.stringify(data.revBillBean)],
-        ["sentBillBean", JSON.stringify(data.sentBillBean)],
-        ["filterBeans", JSON.stringify(data.filterBeans)],
-        ["userInfoBean", JSON.stringify(data.userInfoBean)],
-        ["token", JSON.stringify(data.token)],
-        ["certifiedOrgBean", JSON.stringify(!data.certifiedOrgBean ? '' : data.certifiedOrgBean)],
-        ["mainMsgBean", JSON.stringify(data.mainMsgBean)],
-        ["marketMsgBeans", JSON.stringify(data.marketMsgBeans)],
-        ["systemMsgBeans", JSON.stringify(data.systemMsgBeans)],
-        ["sentBillMsgBeans", JSON.stringify(data.sentBillMsgBeans)],
-        ["demoFlag", JSON.stringify(data.demoFlag)],
-        ["newOrg", JSON.stringify(!data.newOrg ? '' : data.newOrg)]
-    ])
-}
+    if (tokenData.length > 0) {
+        let _persisterToken = tokenData[0];
+        let _values = _persisterToken.values;
+
+        _values.map((item, index) => {
+            _appObject[item.key] = JSON.parse(item.value);
+        });
+
+        let _flags = _persisterToken.flags.filtered('key == "demoFlag"');
+        _.assign(_appObject, {
+            token: _persisterToken.token,
+            demoFlag: _flags[0]
+        });
+    }
+
+    if (cb) cb(_appObject);
+};
+
+let _saveAPNToken = function (_apnToken, cb) {
+    _realm.write(() => {
+        _realm.create(DeviceSchema, {
+            device: DEVICE_ID,
+            APNSToken: _apnToken
+        });
+        if (cb) cb();
+    })
+};
+
+let _saveAppData = function (data) {
+    // Create Realm objects and write to local storage
+    _token = data.token;
+    _realm.write(() => {
+        _realm.create(TokenSchema, {
+            token: data.token,
+            //APNSToken: '',
+            values: [{
+                key: 'revBillBean',
+                value: JSON.stringify(data.revBillBean)
+            }, {
+                key: 'sentBillBean',
+                value: JSON.stringify(data.sentBillBean)
+            }, {
+                key: 'filterBeans',
+                value: JSON.stringify(data.filterBeans)
+            }, {
+                key: 'userInfoBean',
+                value: JSON.stringify(data.userInfoBean)
+            }, {
+                key: 'certifiedOrgBean',
+                value: JSON.stringify(data.certifiedOrgBean)
+            }, {
+                key: 'newOrg',
+                value: JSON.stringify(data.newOrg)
+            }, {
+                key: 'mainMsgBean',
+                value: JSON.stringify(data.mainMsgBean)
+            }, {
+                key: 'marketMsgBeans',
+                value: JSON.stringify(data.marketMsgBeans)
+            }, {
+                key: 'systemMsgBeans',
+                value: JSON.stringify(data.systemMsgBeans)
+            }, {
+                key: 'sentBillMsgBeans',
+                value: JSON.stringify(data.sentBillMsgBeans)
+            }],
+            flags: [{
+                key: 'demoFlag',
+                flag: Boolean(data.demoFlag)
+            }]
+        });
+    });
+};
 
 
-module.exports = Actions;
+let PersisterFacade = {
+    getAppData: (cb) => _getAppData(cb),
+    saveAppData: (data) => _saveAppData(data),
+    clearToken: () => _clearToken(),
+    saveAPNSToken: (apnsToken, cb) => _saveAPNToken(apnsToken, cb),
+    setItem: (k, v, c) => _setValue(k, v, c),
+    saveUser: (user, cb) => _setValue('userInfoBean', user, cb),
+    saveOrg: (org, cb) => _setValue('certifiedOrgBean', org, cb),
+    saveMsgDetail: (mainMsgBean, cb) => _setValue('mainMsgBean', mainMsgBean, cb),
+    saveMainMsgBean: (mainMsgBean, cb) => _setValue('mainMsgBean', mainMsgBean, cb),
+    saveDemoFlag: (flag, cb) => _setFlag('demoFlag', flag, cb),
+};
+
+module.exports = PersisterFacade;
