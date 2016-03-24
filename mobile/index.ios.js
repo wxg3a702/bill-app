f2a0366f92fed655ff9319dfd39a1f994c9fb95a
xgwen@amarsoft.com <wxg3a702@163.com>
2016-03-24 15:24:22 +0800

    代码合并

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -1,4 +1,4 @@
 var React = require('react-native');
 var { AppRegistry } = React;
 var FasApp = require('./content/framework/system/navigator');
-AppRegistry.registerComponent('mobile', () => FasApp);
+AppRegistry.registerComponent('FasApp', () => FasApp);
